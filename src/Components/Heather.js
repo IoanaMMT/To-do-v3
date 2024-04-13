62d0962b103b2accb2a28bc68f968a03db4e626a
IoanaMMT <34572741+IoanaMMT@users.noreply.github.com>
2024-04-13 09:10:22 +0100

    add

--- src/Components/Heather.js
+++ src/Components/Heather.js
@@ -1,25 +1,25 @@
-import * as React from 'react';
+import * as React from "react";
 
 export default function DisplayHeather() {
+  const now = new Date();
 
-const now = new Date();
+  const year = now.getFullYear();
+  const month = now.getMonth() + 1; // Note: Month is zero-based, so add 1
+  const day = now.getDate();
 
-const year = now.getFullYear();
-const month = now.getMonth() + 1; // Note: Month is zero-based, so add 1
-const day = now.getDate();
+  // get the current date and time as a string
+  const currentDate = `${day < 10 ? "0" + day : day}-${
+    month < 10 ? "0" + month : month
+  }-${year}`;
 
-// get the current date and time as a string
-const currentDate = `${day < 10 ? '0' + day : day}-${month < 10 ? '0' + month : month}-${year}`;
+  // now.toLocaleString();
 
-// now.toLocaleString();
-    
-    return (
-        <>
-            <div className='top_row'>
-                <h2 id='my_day'>My Day</h2>
-                 <h3 id='today_date'>{currentDate}</h3>
-            </div>
-        </>
-
-    )
+  return (
+    <>
+      <div className="top_row">
+        <h2 id="my_day">My Day</h2>
+        <h3 id="today_date">{currentDate}</h3>
+      </div>
+    </>
+  );
 }
